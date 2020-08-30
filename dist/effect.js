fa3693160f6278cfe5aab2f94c2fd8a2bdf3550d
Hoang Do <katievincy226@gmail.com>
2020-08-30 04:37:23 -0400

    Commit

--- dist/effect.js
+++ dist/effect.js
@@ -32,13 +32,15 @@ let cardArray = [
     "popUp31"
 ];
 
-let cardExist = ""; 
+let cardExist = "";
 let randomNumber = 0;
 let popup = 0;
 let generateCard = 0;
 
+const todoButton = document.querySelector(".btn");
+const todoList = document.querySelector(".todo-list");
 
-
+todoButton.addEventListener("click", addTodo);
 
 function cardPopUp() {
     // Choose one at random from the array 
@@ -46,30 +48,56 @@ function cardPopUp() {
     // Display the chosen one 
 
     //Generate a number and grab a card from it.
-    randomNumber = Math.floor(Math.random() * (cardArray.length - 1)); 
+    randomNumber = Math.floor(Math.random() * (cardArray.length - 1));
     generateCard = cardArray[randomNumber];
 
     //If the card exists, find a new card
-    while(cardExist == generateCard){
-        randomNumber = Math.floor(Math.random() * (cardArray.length - 1)); 
+    while (cardExist == generateCard) {
+        randomNumber = Math.floor(Math.random() * (cardArray.length - 1));
         console.log(randomNumber);
         generateCard = cardArray[randomNumber];
-    }    
+    }
 
     // If the card exists, toggle it off 
-    if(cardExist !== "" && cardExist !== generateCard){
+    if (cardExist !== "" && cardExist !== generateCard) {
         popup = document.getElementById(cardExist);
         popup.classList.remove("show");
-    } 
+    }
 
     //Display the card 
     popup = document.getElementById(generateCard);
-    cardExist = generateCard; 
+    cardExist = generateCard;
     popup.classList.toggle("show");
-    
-    
+
+
 }
 
+const todoCard = document.getElementById(cardExist);
+
+function addTodo(event) {
+    event.preventDefault();
+    //console.log("hello");
+    //todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = cardExist;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //Add todo to storage
+    // saveLocalTodos(todoCard.value);
+    //complete button//del button
+    const starredBtn = document.createElement("button");
+    starredBtn.innerHTML = '<i class="fas fa-star" ></i>';
+    starredBtn.classList.add("starred-btn");
+    todoDiv.appendChild(starredBtn);
+    //append to list
+    todoList.appendChild(todoDiv);
+
+    //clear todo input value
+    todoCard.value = "";
+}
 // function myFunction() {
 //     var x = document.getElementById("yourPopup");
 //     if (x.style.display === "none") {
